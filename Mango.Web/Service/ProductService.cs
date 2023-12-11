6d21755ace266f23e38a912e23fab5bef63fce45
Nikolay Doychev <nikd@sitecore.net>
2023-12-11 17:55:48 +0200

    Publish to Azure

--- Mango.Web/Service/ProductService.cs
+++ Mango.Web/Service/ProductService.cs
@@ -40,15 +40,6 @@ namespace Mango.Web.Service
             });
         }
 
-        public async Task<ResponseDto?> GetProductAsync(string productCode)
-        {
-            return await _baseService.SendAsync(new RequestDto()
-            {
-                ApiType = SD.ApiType.GET,
-                Url = SD.ProductAPIBase + "/api/product/GetByCode/" + productCode,
-            });
-        }
-
         public async Task<ResponseDto?> GetProductBydIdAsync(int productId)
         {
             return await _baseService.SendAsync(new RequestDto()
